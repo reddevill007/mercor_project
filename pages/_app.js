2cd3a86be4a193f4f8c5c435686546d476998a3e
reddevill007 <shivampandey2900@gmail.com>
2023-06-19 19:57:20 +0530

    header

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../src/theme";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>Mercor Project</title>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
